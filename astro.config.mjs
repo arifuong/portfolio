6f0bc47fecbf1337d4c30011e2a6b5edbab32837
arifuong <arifuong04@gmail.com>
2026-08-06 07:11:31 +0700

    Update portfolio animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,19 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://portfolio-zeta-navy-gbloxdhna7.vercel.app',
   integrations: [sitemap()],
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssMinify: 'lightningcss',
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+        },
+      },
+    },
   },
 });
